e2643263c86e16918008b11ec3f306eb8755623d
Menuka <menuka.14@cse.mrt.ac.lk>
2017-02-09 23:12:30 +0530

    Add Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
